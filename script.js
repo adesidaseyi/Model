12271a96c881d72d4da2dd4a33bf2aa356ca82aa
adesidaseyi <adesida.seyi@gmail.com>
2025-04-24 08:53:51 +0100

    updated annotations and buttons

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ document.querySelector('#color-controls').addEventListener('click', (event) => {
   const [material] = modelViewerColor.model.materials;
   material.pbrMetallicRoughness.setBaseColorFactor(colorString);
 });
+
+/*
+                  <button class="view-button" slot="hotspot-10" data-position="0.02610224m 0.01458751m -0.004978945m" data-normal="-0.602551m 0.7856147m -0.1405055m" data-orbit="-78.89725deg 77.17752deg 0.08451112m" data-target="0.02610223m 0.0145875m -0.004978945m">
+                    Treasure
+                  </button>
+                  <button class="view-button" slot="hotspot-11" data-position="-0.1053838m 0.01610652m 0.1076345m" data-normal="-0.624763m 0.5176854m 0.5845283m" data-orbit="10.89188deg 119.9775deg 0.03543022m" data-target="-0.1053838m 0.01610652m 0.1076345m">
+                    Desperation
+                  </button>
+                </model-viewer>
+
+                <script type="module">
+                  const modelViewer2 = document.querySelector("#hotspot-camera-view-demo");
+                  const annotationClicked = (annotation) => {
+                    let dataset = annotation.dataset;
+                    modelViewer2.cameraTarget = dataset.target;
+                    modelViewer2.cameraOrbit = dataset.orbit;
+                    modelViewer2.fieldOfView = '45deg';
+                  }
+
+                  modelViewer2.querySelectorAll('button').forEach((hotspot) => {
+                    hotspot.addEventListener('click', () => annotationClicked(hotspot));
+                  });
+*/
